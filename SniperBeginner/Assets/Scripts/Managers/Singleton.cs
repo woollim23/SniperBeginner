8b343a59330e6640038ef356ccdbd1dfeb5b6720
woollim <137277221+woollim23@users.noreply.github.com>
2024-11-15 12:46:29 +0900

    [Complex] 인풋시스템, 시네머신, 2D 스프라이트 추가, 레이어 및 태그 추가 등

---
+++ SniperBeginner/Assets/Scripts/Managers/Singleton.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<T>();
+
+                if (instance == null)
+                {
+                    instance = new GameObject(typeof(T).Name).AddComponent<T>();
+                }
+            }
+
+            return instance;
+        }
+    }
+}
