bd6f064c4643af133c409a64eeb0ada8e684191e
kkamsogod <9310rlawlgns@gmail.com>
2024-11-15 17:58:17 +0900

    [Feature] 스크립터블 오브젝트 무기,아이템 / 각 데이타 추가

---
+++ SniperBeginner/Assets/ScriptableObjects/DataEnums.cs
@@ -0,0 +1,18 @@
+public enum WeaponType
+{
+    Pistol,
+    Sniper
+}
+
+public enum AmmoType
+{
+    ACP45,
+    Magnum338
+}
+
+public enum ItemType
+{
+    Heal,
+    SpeedBoost,
+    Silencer
+}
