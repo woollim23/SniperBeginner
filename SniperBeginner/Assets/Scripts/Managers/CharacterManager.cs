d8a259808c9ba9434817f3e022323b4898942b4b
jhk0701 <134210309+jhk0701@users.noreply.github.com>
2024-11-20 21:53:43 +0900

    [Fix] merg 직후 버그 수정

--- SniperBeginner/Assets/Scripts/Managers/CharacterManager.cs
+++ SniperBeginner/Assets/Scripts/Managers/CharacterManager.cs
@@ -1,25 +1,54 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class CharacterManager : Singleton<CharacterManager>
 {
     [SerializeField] Player playerPrefab;
+    [SerializeField] public List<GameObject> enemies;
     public Player Player { get; set; }
 
     [SerializeField] Enemy enemyPrefab;
     [SerializeField] Transform[] SpawnPoints;
 
+    private void Start() 
+    {
+        if(!Player)
+            Instantiate(playerPrefab);
+
+        InstaiateEnemy();
+    }
+
     public void Initialize()
     {
         Player = Instantiate(playerPrefab);
+        Player.Initialize(null); // TODO : ·ÎµåÇÑ µ¥ÀÌÅÍ ¹Ş¾Æ¿À±â
 
-        // ì¶”í›„ì— ì ë“¤ë„ ìƒì„±
+        InstaiateEnemy();
     }
 
     public void InstaiateEnemy()
     {
-        Enemy e = Instantiate(enemyPrefab);
-        // e.Agent.Warp(Position);
-        
+        if (false)
+        {
+            // TODO : ????? ??????? ???? ?? ???? ??? ?????
+            // ??????????? ????? ??????? ??? ????
+            for (int i = 0; i < SpawnPoints.Length; i++)
+            {
+                Enemy newEnemy = Instantiate(enemyPrefab, SpawnPoints[i].position, SpawnPoints[i].rotation);
+
+                // ???? ?????? ????? ???????? ????
+                if (newEnemy.Health <= 0)
+                    GameManager.Instance.Score++;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < SpawnPoints.Length; i++)
+            {
+                Enemy newEnemy = Instantiate(enemyPrefab, SpawnPoints[i].position, SpawnPoints[i].rotation);
+                enemies.Add(newEnemy.gameObject);
+            }
+        }
     }
 
 }
