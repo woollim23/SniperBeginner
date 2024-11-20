e02c22469ac6a218bb13cc4258843d046dfc0601
kkamsogod <9310rlawlgns@gmail.com>
2024-11-20 11:58:52 +0900

    [Feature] 이넘컬렉션 추가

---
+++ SniperBeginner/Assets/Scripts/EnumCollection.cs
@@ -0,0 +1,27 @@
+public enum WeaponType
+{
+    Pistol,
+    Rifle,
+    Sniper
+}
+
+public enum AmmoType
+{
+    PistolAmmo,
+    RifleAmmo,
+    SniperAmmo
+}
+
+public enum ItemType
+{
+    Heal,
+    PistolMagazine,
+    SniperMagazine
+}
+
+public enum ParticleType
+{
+    MuzzleFlash,
+    WallImpact,
+    BloodImpact
+}
