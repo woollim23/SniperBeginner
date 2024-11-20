01b67563fc56b097202d9c2601e81d3c653f2b3e
woollim <137277221+woollim23@users.noreply.github.com>
2024-11-20 21:31:33 +0900

    [Update] Enemy 스폰 형식으로 변경, Gamemanager 점수 카운팅 방법 변경

--- SniperBeginner/Assets/Scripts/Managers/CharacterManager.cs
+++ SniperBeginner/Assets/Scripts/Managers/CharacterManager.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CharacterManager : Singleton<CharacterManager>
 {
     [SerializeField] Player playerPrefab;
+    [SerializeField] public List<GameObject> enemies;
     public Player Player { get; set; }
 
     [SerializeField] Transform[] SpawnPoints;
@@ -10,15 +13,33 @@ public class CharacterManager : Singleton<CharacterManager>
 
     private void Start() 
     {
-        if(!Player)
-            Instantiate(playerPrefab);
+        if (!Player) Instantiate(playerPrefab);
+        InstaiateEnemy();
     }
 
     public void InstaiateEnemy()
     {
-        Enemy e = Instantiate(enemyPrefab);
-        // e.Agent.Warp(Position);
-        
+        if (false)
+        {
+            // TODO : 저장된 리스트의 카운트 수 만들 반복 되도록
+            // 저장데이터에서 위치값 불러오고 체력 적용
+            for (int i = 0; i < SpawnPoints.Length; i++)
+            {
+                Enemy newEnemy = Instantiate(enemyPrefab, SpawnPoints[i].position, SpawnPoints[i].rotation);
+
+                // 처치된 에너미면 스코어가 오르도록 수정
+                if (newEnemy.Health <= 0)
+                    GameManager.Instance.Score++;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < SpawnPoints.Length; i++)
+            {
+                Enemy newEnemy = Instantiate(enemyPrefab, SpawnPoints[i].position, SpawnPoints[i].rotation);
+                enemies.Add(newEnemy.gameObject);
+            }
+        }
     }
 
 }
