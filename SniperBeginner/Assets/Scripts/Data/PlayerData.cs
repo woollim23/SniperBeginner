7d17035e940b5e467d2681a7bb26af1ab89dc920
yoonday <yoon2dev@gmail.com>
2024-11-21 12:08:17 +0900

    [Chore] `CurrentAmmoInMagazine` List<int>로 변경
    - 각 총기에 해당하는 총알을 구분하기 위함

--- SniperBeginner/Assets/Scripts/Data/PlayerData.cs
+++ SniperBeginner/Assets/Scripts/Data/PlayerData.cs
@@ -8,5 +8,5 @@ public class PlayerData
     public Vector3 Position;
     public float Health;
     public int EquippedWeaponIndex; // 장비 리스트 사용
-    public int CurrentAmmoInMagazine;
+    public List<int> CurrentAmmoInMagazine; // 장비에 따른 총알 수
 }
